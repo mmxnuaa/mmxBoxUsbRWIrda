4ff8ed9f1fdd54e0e7532101a2f1509beb9e1e3d
zhiwei.meng <zhiwei.meng@jiagu-tech.com>
2018-04-03 11:17:05 +0800

    usb r/w test works

--- jni/usbRW.cpp
+++ jni/usbRW.cpp
@@ -60,6 +60,37 @@ static int discovery_done_cb(void *client_data)
     return true;
 }
 
+static bool is_vid_pid_match(const char *dev_name, uint16_t vid, uint16_t pid)
+{
+    struct usb_device *device = usb_device_open(dev_name);
+    if (device == nullptr){
+        printf("open device %s fail\n", dev_name);
+        return false;
+    }
+    uint16_t usb_vid = usb_device_get_vendor_id(device);
+    uint16_t usb_pid = usb_device_get_product_id(device);
+    printf("%s, %04x:%04x, writeable=%d\n", dev_name, usb_vid, usb_pid, usb_device_is_writeable(device));
+    usb_device_close(device);
+    return usb_vid==vid && usb_pid==pid;
+}
+
+static void test_target(const char *dev_name)
+{
+    struct usb_device *device = usb_device_open(dev_name);
+    if (device == nullptr){
+        printf("open device %s fail\n", dev_name);
+        return ;
+    }
+
+    const char testStr[]= "sdjflksdjsjfj1234";
+    int cnt = usb_device_bulk_transfer(device, 0x1, (void *) testStr, static_cast<int>(strlen(testStr)), 1000);
+    printf("write result: %d\n", cnt);
+    char read[256]= {0};
+    cnt = usb_device_bulk_transfer(device, 0x81, (void *) read, 200, 1000);
+    printf("read result: %d, %s\n", cnt, read);
+
+    usb_device_close(device);
+}
 
 int main()
 {
@@ -77,8 +108,24 @@ int main()
                       discovery_done_cb,
                       &head);
 
-    printf("done");
+    printf("done query\n");
     usb_host_cleanup(usb_ctx);
+
+    DevInfoNode *Node = head;
+    const char *target = nullptr;
+    while (Node != nullptr){
+        if (is_vid_pid_match(Node->dev_name, 0x0483, 0x5740)){
+            printf("match: %s\n", Node->dev_name);
+            target = Node->dev_name;
+        }
+        Node = Node->next;
+    }
+
+    if (target != nullptr){
+        test_target(target);
+    }
+
     free_list(&head);
     return 0;
 }
+
